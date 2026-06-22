e19e00eb5edb3788eceb593be9c3c60873d1d46b
aminhamki <295626296+aminhamki@users.noreply.github.com>
2026-06-22 13:41:04 +0200

    Update timer.gd

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -13,17 +13,17 @@ func _process(delta):
 
 func _input(event):
 
-	# START erst beim Loslassen
+	
 	if !timer_running and can_start and event.is_action_released("ui_accept"):
 		elapsed_time = 0
 		start_time = Time.get_ticks_msec()
 		timer_running = true
 
-	# STOP sofort beim Drücken
+	
 	elif timer_running and event.is_action_pressed("ui_accept"):
 		timer_running = false
 		can_start = false
 
-	# Nach dem Loslassen wieder startbereit
+	
 	elif !timer_running and event.is_action_released("ui_accept"):
 		can_start = true
